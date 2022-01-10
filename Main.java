0515258e39d1b96c675be479f900231367fc6c2e
logicpratik <97430806+logicpratik@users.noreply.github.com>
2022-01-10 22:47:56 +0530

    Add files via upload
    String Reverse..
    Logic was to get a string...store it as individual characters i.e an array..then print it using for loop (in reverse)
    checked out how you store string in an array..thats using getChar

--- Main.java
+++ Main.java
@@ -4,77 +4,22 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void printareaperimeter()
-    {        int length;
-             int width;
-
-     Scanner sc = new Scanner(System.in);
-            System.out.println("Provide Length");
-            length = sc.nextInt();
-        if (length>0)
-           //System.out.println("Length cannot be negative");
-           //     else
-        System.out.println("Provide width");
-            width = sc.nextInt();
-//if (width > 0)
-        //if (width > 0)
-
-      //  if (width<0)
-
-        //  System.out.println("width cannot be negative");
-
-          //else
-            int area = length*width;
-            int perimeter = length+width;
-            System.out.print("Perimeter=");
-        System.out.println(perimeter);
-        System.out.print("Area=");
-        System.out.println(area);
-        //else
-        //System.out.println("Input cannot be nagative");
-        //break;
-    }
-  //  public static void perimeter()
-    //{
-      //  int length;
-        //int width;
-
-        //Scanner sc = new Scanner(System.in);
-        //System.out.println("Provide Length");
-        //length = sc.nextInt();
-        //System.out.println("Provide width");
-        //width = sc.nextInt();
-        //int perimeter = length+width;
-        //System.out.print("perimeter=");
-        //System.out.println(perimeter);
-
-
-
     public static void main(String[] args) {
 	// write your code here
-        //to provide square of the user given number
+//        String name[] = new String[6];
+  //      String input;
+    //    Scanner input = new Scanner(System.in);
 
-       // public static void printarea(int length,int width)
-    //{ int area = length * width;
-//        System.out.println(area);}
-
-        //Scanner sc = new Scanner(System.in);
-        //do {
-          //  System.out.println("Provide Length");
-            //length = sc.nextInt();
-            //System.out.println("Provide width");
-            //width = sc.nextInt();
-           //if (length < 0)
-             //  System.out.println("Length or Width cannot be negative");
-
-           // else
-             //printarea();
-
-
-//break;
-
-//        }while (area < 1000000);
-printareaperimeter();
-
+      //  System.out.println("What is your name?");
+char character;
+      //  character = 'r';
+        //character++;
+        //System.out.println(character);
+String str = "Pratik Patwardhan";
+      //  char name[] = new char[40];
+        //str.getChars(0,6,name,0);
+        //for (int i=5;i>=0;i--)
+        //{System.out.print(name[i]);
+    //IT TAKES PRATIK BUT NOT PATWARDHAN WHICH IS AFTER A space
     }
-}
+}}
