37ffaa1f7eb972e086588407d05c68b0862eb6e4
logicpratik <97430806+logicpratik@users.noreply.github.com>
2022-01-12 18:14:40 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,21 +5,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        String name[] = new String[6];
-  //      String input;
-    //    Scanner input = new Scanner(System.in);
 
-      //  System.out.println("What is your name?");
-char character;
-      //  character = 'r';
-        //character++;
-        //System.out.println(character);
-String str = "Pratik Patwardhan";
-      //  char name[] = new char[40];
-        //str.getChars(0,6,name,0);
-        //for (int i=5;i>=0;i--)
-        //{System.out.print(name[i]);
-    //IT TAKES PRATIK BUT NOT PATWARDHAN WHICH IS AFTER A space
+        String str = "Pratik Patwardhan";
+        char name[] = new char[40];
+        str.getChars(0, 17, name, 0);
+        for (int i = 16; i >= 0; i--) {
+            {
+                System.out.print(name[i]);
+            }
+        }
     }
-}}
+}
