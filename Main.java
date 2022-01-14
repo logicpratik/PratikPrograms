da0f749045cf0d566f3bb04ac18d4fe71750377b
logicpratik <97430806+logicpratik@users.noreply.github.com>
2022-01-14 20:12:03 +0530

    Add files via upload
    1
    1D Arrays..input from user and display

--- Main.java
+++ Main.java
@@ -1,24 +1,27 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        //mirror pyramid
-        int n = 4;
-        for (int i = 1; i <= n; i++) {
-//printing space
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-//printing stars
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("what is the number of subjects");
+        int size = input.nextInt();
+
+        int marks[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            System.out.println("What is the marks in subject" + (i + 1));
+            marks[i] = input.nextInt();
+            //  System.out.println("Marks in subject" + i + 1 + "is" + marks[i]);
+
+
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.println("Marks in subject " + (i + 1) + " are " + marks[i]);
         }
     }
 }
-
-
-
